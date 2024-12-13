ccf4c7eb08661bdf538d553d07d47f0bc7980d4b
Mahdi Rahman <mahdir246@gmail.com>
2024-12-13 12:12:41 -0500

    finish part 2 day 7

--- 7/part1.py
+++ 7/part1.py
@@ -2,33 +2,24 @@ res = 0
 
 
 def target_possible(nums, target):
-    curr_set = set()
-    def dfs(i, res, curr_set):
+
+    def dfs(i, res):
         if i == len(nums):
-            curr_set.add(res)
-            return
+            return res == target
         
-        dfs(i+1, res * nums[i], curr_set)
-        dfs(i+1, res + nums[i], curr_set)
-        dfs(i+1, nums[i], curr_set)
-
-        str_res = str(res)
-        remain = str(target)[len(str_res):]
-        if remain != '' and int(remain) in curr_set  and int(str_res + remain) == target:
-            return True
-
-
-    res = dfs(1, nums[0], curr_set)
-    if res or target in curr_set:
-        return True
-    return False
+        return dfs(i+1, res * nums[i]) or dfs(i+1, res + nums[i]) or dfs(i+1, int(str(res)+str(nums[i])))
+    
+         
+      
+    return dfs(1, nums[0])
 
 with open("input.txt", "r") as filename:
     for line in filename.readlines():
         target = int(line.rstrip().split(":")[0])
         nums = line.rstrip().split(": ")[1].split(" ")
         nums = [int(num) for num in nums]
-        res += target_possible(nums, target)
+        if target_possible(nums, target):
+            res += target
         
     
 
